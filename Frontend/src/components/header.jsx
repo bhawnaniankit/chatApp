6b4726fc7dc08615e6557d2286c42416c5c16b39
bhawnaniankit <bhawnaniankit@gmail.com>
2024-03-17 15:52:42 +0530

    Header initialized

---
+++ Frontend/src/components/header.jsx
@@ -0,0 +1,12 @@
+export function Header() {
+    return <div className=" bg-black ">
+        <nav>
+            <ul className="flex ">
+                <li>Home</li>
+                <li>LogIn</li>
+                <li>SignUp</li>
+                <li>Contact Us</li>
+            </ul>
+        </nav>
+    </div>
+}
--- Frontend/src/pages/home/home.jsx
+++ Frontend/src/pages/home/home.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Sidebar } from '../../components/Sidebar/Sidebar';
 import MessageContainer from "../../components/messages/MessageContainer.jsx"
+import { Header } from '../../components/header.jsx';
 
 const Home = () => {
     return (
         <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-            <Sidebar />
+            <div>
+                <Header></Header>
+                <Sidebar />
+
+                {/* <div>
+                </div> */}
+            </div>
             <MessageContainer />
         </div>
     )
